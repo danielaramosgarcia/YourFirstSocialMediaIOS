8cd53c5001944a4c6c9ee7a44d169f45d4f8db4c
Claudia Arcienega <126215454+carcienegam@users.noreply.github.com>
2023-12-06 23:08:28 -0600

    view model cambios

--- YourFirstSocialMedia/Home/HomeView.swift
+++ YourFirstSocialMedia/Home/HomeView.swift
@@ -13,50 +13,18 @@ struct HomeView: View {
     @EnvironmentObject var homeVM: HomeViewModel
     
     var body: some View {
-            VStack {
-                ZStack {
+        VStack {
+            ZStack {
 
-                    ScrollView(.vertical, showsIndicators: false) {
-                        ForEach(homeVM.arrHome) { item in
-                            PostView(home: item)
-                                .foregroundColor(Color(.black))
-                        }
+                ScrollView(.vertical, showsIndicators: false) {
+                    ForEach(homeVM.arrHome) { item in
+                        PostView(home: item)
+                            .foregroundColor(Color(.black))
                     }
-                    
-                    VStack {
-                        Spacer()
-                        
-                        HStack {
-                            Spacer()
-                            
-                            Circle()
-                                .foregroundColor(Color.clear)
-                                .frame(width: 60, height: 60)
-                            
-                            Button(action: {
-
-                            }) {
-                                Image(systemName: "plus")
-                                    .resizable()
-                                    .frame(width: 25, height: 25)
-                                    .padding()
-                                    .background(Color("AccentColor"))
-                                    .foregroundColor(.white)
-                                    .cornerRadius(50)
-                            }
-                            .padding()
-                            .frame(width: 60, height: 60)
-                            .contentShape(Circle())
-                        }
-                        .padding(.horizontal, 15)
-                        .padding(.bottom, 25)
-                    }
-                    
                 }
-                
+            }
         }
     }
-   
 }
 
 
--- YourFirstSocialMedia/Home/HomeViewModel.swift
+++ YourFirstSocialMedia/Home/HomeViewModel.swift
@@ -20,19 +20,13 @@ class HomeViewModel : ObservableObject{
         
         var home : HomeModel
         
-        home = HomeModel(titulo: "FemCoding Challenge 2023 ", descripcion: "Hoy volamos a Tijuana en camino al FemCoding Challenge 2023 organizado por Enactus. Estamos super emocionadas de por fin poderles mostrar nuestra app a todos. USinSTEM! @danielaramosgarcia @valelimon", nombreImagenPost: "profileClaudia", NombrePersona: "@clauarcienegam", nombreImagen: "profileClau", questions: 8)
+        home = HomeModel(titulo: "La última cena", descripcion: "Les enseño la hermosa obra que hice hoy.", nombreImagenPost: "davinciPost1", NombrePersona: "@davinci", nombreImagen: "davinciProfile", questions: 8)
         arrHome.append(home)
         
-        home = HomeModel(titulo: "Fin del internship en Uber, SF 🌉", descripcion: "Cierro mi etapa como ingeniera de software en Uber. Gracias por las experiencias desafiantes y el aprendizaje en este emocionante viaje. ¡Listos para el próximo desafío! 🚗✨ ", nombreImagenPost: "PostDani", NombrePersona: "@danielaramosgarcia", nombreImagen: "yo", questions: 30)
+        home = HomeModel(titulo: "Mona Lisa", descripcion: "Pintura iconica de la Mona Lisa", nombreImagenPost: "davinciPost2", NombrePersona: "@davinci", nombreImagen: "davinciProfile", questions: 30)
         arrHome.append(home)
         
-        home = HomeModel(titulo: "Evento", descripcion: "💻🚀 Únete a nuestro equipo. Buscamos ingenieras apasionadas para liderar proyectos innovadores. Tu talento es la clave para construir el futuro. ¡Aplica ahora! ", nombreImagenPost: "PostDani2", NombrePersona: "EmpresaX", nombreImagen: "fakecompany", questions: 3)
-        arrHome.append(home)
-        
-        home = HomeModel(titulo: "Oportunidades para mujeres", descripcion: "💻🚀 Únete a nuestro equipo. Buscamos ingenieras apasionadas para liderar proyectos innovadores. Tu talento es la clave para construir el futuro. ¡Aplica ahora! ", NombrePersona: "EmpresaX", nombreImagen: "fakecompany", questions: 3)
-        arrHome.append(home)
-        
-        home = HomeModel(titulo: "CyberHackathon", descripcion: "La semana pasada, participé en un emocionante hackathon. Desafíos superados y nuevos aprendizajes. ¡La seguridad en línea es tarea de todos! 👩‍💻🌐.", nombreImagenPost: "PostVale", NombrePersona:"@valelimon", nombreImagen: "vale", questions: 28)
+        home = HomeModel(titulo: "CyberHackathon", descripcion: "La semana pasada, participé en un emocionante hackathon. Desafíos superados y nuevos aprendizajes. ¡La seguridad en línea es tarea de todos! 👩‍💻🌐.", nombreImagenPost: "PostVale", NombrePersona:"@valelimon", nombreImagen: "davinciProfile", questions: 28)
         arrHome.append(home)
         
          home = HomeModel(titulo: "Speech: Mujeres en STEM ", descripcion: "Ven a este Speech acerca de la importancia de la igualdad de género en el ámbito laboral.", nombreImagenPost: "PostSpeech", NombrePersona: "@CCMDev", nombreImagen: "SpeechCCMDev", questions: 12)
