87acbce920e25b907c11f91e9011bd8321512a75
Daniela Ramos <daniramos5566@hotmail.com>
2023-12-06 18:18:13 -0600

    Initial Commit

---
+++ YourFirstSocialMedia/YourFirstSocialMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourFirstSocialMediaApp.swift
+//  YourFirstSocialMedia
+//
+//  Created by Daniela Ramos Garcia on 06/12/23.
+//
+
+import SwiftUI
+
+@main
+struct YourFirstSocialMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
